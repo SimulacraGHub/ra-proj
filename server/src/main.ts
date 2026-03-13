58b146f1b36b948a1c3163e5d9a471cab23b1aba
SimulacraGHub <opethfan78@gmail.com>
2026-03-13 13:11:34 +0200

    Updated listening ports and network interface

--- server/src/main.ts
+++ server/src/main.ts
@@ -53,7 +53,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../react-app/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
